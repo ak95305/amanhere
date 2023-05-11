1dbfb4d28aef3521ce91223d6778979ce10acce1
ak669212@gmail.com <ak95305>
2023-05-11 08:32:30 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// jQuery(document).on('scroll', function () {
+//     scrollValue = Math.round(scrollY);
+
+//     if(scrollValue > 350){
+//         console.log(scrollValue);
+//         jQuery('#move').css("left", Math.max(100 - 0.2 * window.scrollY, 1) + "vw");
+//     }
+// })
